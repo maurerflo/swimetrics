dc902c98f42963b05be65abc1327343c0e351a6e
Niklas Kappler <mail@niklas-kappler.de>
2024-11-24 16:52:56 +0100

    feat: setup basic layout structure

--- apps/video-analyzer/src/app.vue
+++ apps/video-analyzer/src/app.vue
@@ -1,44 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <header>
-      <nav>
-        <NuxtLink to="/"> Home </NuxtLink>
-        <NuxtLink to="/about"> About </NuxtLink>
-      </nav>
-    </header>
-    <nuxt-page />
-  </main>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
 
-<style scoped lang="css">
-header {
-  line-height: 1.5;
-  max-width: 100vw;
-}
+<style scoped lang="scss">
 
-nav > a {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-
-@media (min-width: 768px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 768px;
-  }
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ apps/video-analyzer/src/layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <AppHeader />
+    <main class="flex-grow flex overflow-hidden">
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<style scoped></style>
